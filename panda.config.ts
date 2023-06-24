150b294a48f4421db15924bda780cecf0bb704b7
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-06-24 23:11:21 +0200

    Repro

---
+++ panda.config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  studio: {
+    logo: "https://place-hold.it/36x24",
+  },
+  preflight: true,
+  include: ["./src/**/*.{js,svelte,ts}"],
+  outdir: "styled-system",
+  theme: {
+    semanticTokens: {
+      colors: {
+        text: {
+          value: { base: "{colors.gray.600}", _osDark: "{colors.gray.400}" },
+        },
+      },
+    },
+    recipes: {
+      button: {
+        name: "button",
+        description: "A button styles",
+        base: {
+          fontSize: "lg",
+        },
+        variants: {
+          size: {
+            sm: {
+              padding: "2",
+              borderRadius: "sm",
+            },
+            md: {
+              padding: "4",
+              borderRadius: "md",
+            },
+          },
+          variant: {
+            primary: {
+              color: "white",
+              backgroundColor: "blue.500",
+            },
+            danger: {
+              color: "white",
+              backgroundColor: "red.500",
+            },
+          },
+        },
+      },
+    },
+  },
+  globalCss: {
+    "*": {
+      fontFamily: "Inter",
+      margin: "0",
+    },
+    a: {
+      color: "inherit",
+      textDecoration: "none",
+    },
+  },
+});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  let styleModule = css({ color: "yellow.400" });
+</script>
+
+<script>
+  import { css } from "styled-system/css";
+
+  let style = css({ color: "green.400" });
+</script>
+
+<h1 class={style}>using class binding css</h1>
+<div
+  class={css({
+    padding: "40px",
+    color: "red.700",
+    fontSize: "3xl",
+  })}
+>
+  It works in Svelte, using inline styles
+</div>
+<p class={styleModule}>
+  It doesn't work in Svelte using script context binding
+</p>
